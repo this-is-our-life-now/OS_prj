958c28fa136a8d35fd685d6243938a407afc79d9
Megan Cash <mecfkc@mst.edu>
2019-04-24 21:35:09 -0500

    setting up file structure and ideas

---
+++ os_main.c
@@ -0,0 +1,25 @@
+/*----------------------------------------------
+Katie Pitz, Paige Huffman, Megan Cash
+OS Final Project
+Simple OS for Arudino
+----------------------------------------------*/
+
+/*----------------------------------------------
+TODO:
+    >process management/threading/scheduling
+    >some sort of file sys on SD card
+    >timer along with 7 segment display
+    >LCD display for statuses
+    >some sort of input thing for fun so we
+    have a reason to write to files/log stuff
+----------------------------------------------*/
+
+#include "external.c"
+#include "file_system.c"
+#include "lcd_display.c"
+#include "processing.c"
+
+int main()
+{
+
+}
