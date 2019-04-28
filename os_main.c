a89a717856108f77211727280248658877ccd51d
Megan Cash <mecfkc@mst.edu>
2019-04-27 21:52:15 -0500

    added dht11 sensor

--- os_main.c
+++ os_main.c
@@ -8,10 +8,21 @@ Simple OS for Arduino
 TODO:
     > process management/threading/scheduling
     > some sort of file sys on SD card
-    > timer along with 7 segment display
+    > 7 segment display for process times
     > LCD display for statuses
     > some sort of input sensor stuff so we
     have a reason to write to files/log stuff
+Further Process Ideas:
+    > use remote to trigger processes and log
+        results in their own files, also log
+        process times in its own file maybe
+    > Using DHT11 temp/humidity sensor
+        button 1: log temp
+        button 2: log humidity
+    > using photoresisitor
+        button 3: light val
+    > using tilt switch
+        button 4: tilt val
 -----------------------------------------------*/
 
 #include "external.c"
--- temp_os_prj.ino
+++ temp_os_prj.ino
@@ -1,14 +1,23 @@
 #include <LiquidCrystal.h>
 #include <IRremote.h>
 #include <String.h>
+#include <DHT.h>
+
+// constants
+const int TEMP = 0;
+const int HUMIDITY = 1;
+// probably move all inits into one area like up here
 
 void setup()
 {
-  // serial for testing purposes
-  Serial.begin(9600);
-  
-  lcd_display( "thisisourlifenow" );
-  ir_input();
+    // serial for testing purposes
+    Serial.begin(9600);
+    
+    lcd_display( "thisisourlifenow" );
+    //ir_input();
+    //dht_input( TEMP );
+    dht_input( HUMIDITY );
+
 }
 
 void loop(){}
@@ -29,6 +38,7 @@ void lcd_display( String msg )
     int i = 0; // temporarily have it print once until process handling implemented
     while( i < 1 )
     {
+        Serial.println( "LCD Out:" );
         Serial.println( msg );
         // Turn off the display:
         lcd.display();
@@ -58,12 +68,58 @@ void ir_input()
 {
     ir_receiver.enableIRIn();
 
-    while( true )
+    while( true ) // eventually these won't go infinitely when we handle processes properly
     {
         if ( ir_receiver.decode( &results ) )
         {
             lcd_display( String( results.value, HEX ) );
             ir_receiver.resume();
+        }
+    }
+}
+
+// initialize DHT11 (humidity/temp sensor)
+
+const int dht_pin = 7;
+DHT dht( dht_pin, DHT11 );
+
+/*--------------------------------------------------------
+TODO: if we use dht11 warning this thing is cheap and 
+will give nan values sometimes so we should remember to
+add some sort of abort after waiting a certain amount of
+time(for just this sensor or for all really)
+--------------------------------------------------------*/
+
+void dht_input( int data_type )
+{
+    double input;
+    int valid = false;
+
+    while( true ) // eventually these won't go infinitely when we handle processes properly
+    {
+        if( data_type == TEMP )
+        {
+            while( valid == false )
+            {
+                input = dht.readTemperature();
+                if( !isnan( input ) )
+                {
+                    valid = true;
+                }
+            }
+            lcd_display( String( input ) );
+        }
+        else if( data_type == HUMIDITY )
+        {
+            while( valid == false )
+            {
+                input = dht.readHumidity();
+                if( !isnan( input ) )
+                {
+                    valid = true;
+                }
+            }
+            lcd_display( String( input ) );
         }
     }
 }
