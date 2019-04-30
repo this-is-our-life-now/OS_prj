86765296783bc4a286b132a57838c31179c58a4d
Megan Cash <mecfkc@mst.edu>
2019-04-29 23:13:25 -0500

    round robin working and displays are now nicer

--- temp_os_prj.ino
+++ temp_os_prj.ino
@@ -17,7 +17,7 @@ const int RUNNING   = 1;
 const int DONE      = 2;
 const int ABORT     = 3;
 
-const int RR_TIME = 2000; // round robin time quantum
+const int RR_TIME = 4000; // round robin time quantum
 const int ABORT_TIME = 20000; // give up on processes that have been running for 20s, sadly this may be necessary for my cheap DHT11
 
 // initialize infrared receiver
@@ -41,8 +41,8 @@ struct prcss
 {
     int type;
     int status;
-    int time = millis(); // to keep track of start time
-    int turn_time = millis(); // to keep track of time of that round robin turn
+    float time = millis(); // to keep track of start time
+    float turn_time = millis(); // to keep track of time of that round robin turn
 };
 
 // vector of processes
@@ -58,13 +58,7 @@ void setup()
 
     ir_receiver.enableIRIn();
     
-    lcd_display( "thisisourlifenow", "ft KP,PH,&MC" );
-    lcd_display( "thisisourlifenow", "ft KP,PH,&MC" ); //just cuz
-    //remote_input();
-    //dht_input( TEMP );
-    //dht_input( HUMIDITY );
-    //photo_input();
-
+    lcd_display( "thisisourlifenow", "ft KP,PH,&MC", 1500 );
 }
 
 void loop()
@@ -83,6 +77,8 @@ void loop()
 /*------------------------Processing Section------------------------*/
 void processing()
 {
+    float temp_time;
+
     current.status = RUNNING;
     current.time = buffer[0].time;
     current.type = buffer[0].type;
@@ -102,24 +98,40 @@ void processing()
     }
     else // this really shouldn't happen
     {
-        lcd_display( "err", "unkown process" );
+        lcd_display( "err", "unkown process", 1500 );
     }
 
+    if( ( current.status == WAITING ) && ( buffer.size() > 1 ) )
+    {
+        lcd_display( "RR triggered", "will be back", 1000 );
+    }
     if( current.status == DONE )
     {
         // display process running time
-        lcd_display( "prcss done", String( millis() - current.time ) );
+        temp_time = ( millis() - current.time ) / 1000.0 ;
+        lcd_display( "prcss done", String( temp_time )+"s", 1500 );
         buffer.erase( buffer.begin() );
     }
     if ( current.status == ABORT )
     {
-        lcd_display( "prcss aborted", "took too long" ) ;
+        if( current.type == TEMP_PRCSS )
+        {
+            lcd_display( "temp aborted", "took too long", 1500 ) ;
+        }
+        else if( current.type == HUM_PRCSS )
+        {
+            lcd_display( "humid aborted", "took too long", 1500 ) ;
+        }
+        else
+        {
+            lcd_display( "prcss aborted", "took too long", 1500 );
+        }
         buffer.erase( buffer.begin() );
     }
 }
 
 /*---------------------------LCD Section---------------------------*/
-void lcd_display( String process, String msg )
+void lcd_display( String process, String msg, int time )
 {
     // LCD's number of (columns, rows)
     lcd.begin( 16, 2 );
@@ -130,17 +142,9 @@ void lcd_display( String process, String msg )
     lcd.print( "  " );
     lcd.print( msg );
 
-    // serial for testing purposes
-    Serial.print( process );
-    Serial.println( ":" );
-    Serial.println( msg );
-
     // Turn off the display:
     lcd.display();
-    delay( 1500 );
-    // Turn on the display:
-    lcd.noDisplay();
-    delay( 200 );
+    delay( time );
 }
 
 /*---------------------------Input Section---------------------------*/
@@ -151,19 +155,20 @@ void remote_input()
     incoming.status = WAITING;
 
     code = String( results.value, HEX ) ;
-    lcd_display( "IR Input", code);
 
     // for some reason the remote will return inconsistent values per button
     if( code == "ff30cf" || code == "ee8f7948" )
     {
         incoming.type = PHOTO_PRCSS;
         buffer.push_back( incoming );
+        lcd_display( "IR Input", "photo cell", 1500 );
     }
     else if( code == "ff18e7" || code == "3e9aec57"
         || code == "1af66ed4" || code == "6b3b8e79" )
     {
         incoming.type = TEMP_PRCSS;
         buffer.push_back( incoming );
+        lcd_display( "IR Input", "temp", 1500 );
     }
     else if( code == "ff7a85" || code == "dceecd2c"
         || code == "8dcdae60" || code == "d78c801a"
@@ -171,10 +176,11 @@ void remote_input()
     {
         incoming.type = HUM_PRCSS;
         buffer.push_back( incoming );
+        lcd_display( "IR Input", "humidity", 1500 );
     }
     else
     {
-        lcd_display( "trouble reading", "try again" );
+        lcd_display( "trouble reading", "try again", 1500 );
     }
     
     ir_receiver.resume();
@@ -183,25 +189,27 @@ void remote_input()
 /*--------------------------Sensor Section--------------------------*/
 /*--------------------------------------------------------
 TODO: if we use dht11: warning this thing is cheap and 
-will give nan values sometimes so we should remember to
+will give nan and neagative values sometimes so we should
 add some sort of abort on processes after waiting a
 certain amount of time(for this sensor or for all tbh)
 --------------------------------------------------------*/
 void dht_input( int data_type )
 {
-    double input;
+    double input = 0.0;
     int valid = false;
 
     if( data_type == TEMP )
     {
         while( valid == false )
         {
+            lcd_display( "getting temp", "patience...", 100 );
             input = dht.readTemperature();
-            if( !isnan( input ) )
+            if( !isnan( input ) && input >= 0.0 )
             {
                 valid = true;
             }
-            if( millis() - current.turn_time >= RR_TIME )
+            Serial.println(buffer.size());
+            if( ( millis() - current.turn_time >= RR_TIME ) )
             {
                 current.status = WAITING;
                 return;
@@ -212,18 +220,19 @@ void dht_input( int data_type )
                 return;
             }
         }
-        lcd_display( "DHT Temp", String( input ) );
+        lcd_display( "DHT Temp", String( input )+"C", 1500 );
     }
     else if( data_type == HUMIDITY )
     {
         while( valid == false )
         {
+            lcd_display( "getting humidity", "patience...", 100 );
             input = dht.readHumidity();
             if( !isnan( input ) )
             {
                 valid = true;
             }
-            if( millis() - current.turn_time >= RR_TIME )
+            if( ( millis() - current.turn_time >= RR_TIME ) )
             {
                 current.status = WAITING;
                 return;
@@ -234,13 +243,13 @@ void dht_input( int data_type )
                 return;
             }
         }
-        lcd_display( "DHT Humidity", String( input ) );
+        lcd_display( "DHT Humidity", String( input ), 1500 );
     }
     current.status = DONE;
 }
 
 void photo_input()
 {
-    lcd_display( "Photoresistor", String( analogRead( photo_pin ) ) );
+    lcd_display( "Photoresistor", String( analogRead( photo_pin ) ), 1500 );
     current.status = DONE;
 }
